c3c3495d7400d67fa6a3ea5f81350565c09a94d7
Peter Opiyo <peter.opiyo@student.moringaschool.com>
2021-12-08 17:56:22 +0300

    Names

--- js/index.js
+++ js/index.js
@@ -71,37 +71,37 @@ function getAkanName() {
                 return false;
                 break;
             case 2:
-                document.getElementById('result').textContent = "You were born on a Monday: your Akan name is " + maleAkanNames[1];
+                document.getElementById('result').textContent = "You are a Monday baby: your Akan name is " + maleAkanNames[1];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 3:
-                document.getElementById('result').textContent = "You were born on a Tuesday: your Akan name is " + maleAkanNames[2];
+                document.getElementById('result').textContent = "You are a Tuesday baby: your Akan name is " + maleAkanNames[2];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 4:
-                document.getElementById('result').textContent = "You were born on a Wednesday: your Akan name is " + maleAkanNames[3];
+                document.getElementById('result').textContent = "You are a Wednesday baby: your Akan name is " + maleAkanNames[3];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 5:
-                document.getElementById('result').textContent = "You were born on a Thursday: your Akan name is " + maleAkanNames[4];
+                document.getElementById('result').textContent = "You are a Thursday baby: your Akan name is " + maleAkanNames[4];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 6:
-                document.getElementById('result').textContent = "You were born on a Friday: your Akan name is " + maleAkanNames[5];
+                document.getElementById('result').textContent = "You are a Friday baby: your Akan name is " + maleAkanNames[5];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 7:
-                document.getElementById('result').textContent = "You were born on a Saturday: your Akan name is " + maleAkanNames[6];
+                document.getElementById('result').textContent = "You are a Saturday baby: your Akan name is " + maleAkanNames[6];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
             case 0:
-                document.getElementById('result').textContent = "You were born on a Sunday: your Akan name is " + maleAkanNames[0];
+                document.getElementById('result').textContent = "You are a Sunday baby: your Akan name is " + maleAkanNames[0];
                 document.getElementById('display-name').textContent = "Here is your Akan name: ";
                 return false;
                 break;
